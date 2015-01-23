758f03322a258d0ae39e36c2e7bc97825f69422a
Shubham Chandel <shubham.zeez@gmail.com>
2015-01-23 22:12:32 +0530

    Fixed not-reponding bug: EnterFragment.java

--- app/src/main/java/com/shubham/rural2g/EnterFragment.java
+++ app/src/main/java/com/shubham/rural2g/EnterFragment.java
@@ -57,18 +57,19 @@ public class EnterFragment extends ListFragment {
         userObject = ParseUser.getCurrentUser();
 
         queryTags = ParseQuery.getQuery("tags");
+
         // Query for new results from the network.
-        queryTags.findInBackground(new FindCallback<ParseObject>() {
-            public void done(final List<ParseObject> scores, ParseException e) {
-                // Remove the previously cached results.
-                ParseObject.unpinAllInBackground("", new DeleteCallback() {
-                    public void done(ParseException e) {
-                        // Cache the new results.
-                        ParseObject.pinAllInBackground("", scores);
-                    }
-                });
-            }
-        });
+//        queryTags.findInBackground(new FindCallback<ParseObject>() {
+//            public void done(final List<ParseObject> scores, ParseException e) {
+//                // Remove the previously cached results.
+//                ParseObject.unpinAllInBackground("", new DeleteCallback() {
+//                    public void done(ParseException e) {
+//                        // Cache the new results.
+//                        ParseObject.pinAllInBackground("", scores);
+//                    }
+//                });
+//            }
+//        });
 
         queryTags.whereEqualTo("categoryCode", 1);
 //        queryTags.fromLocalDatastore();
